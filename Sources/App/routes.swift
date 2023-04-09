a187cf8dc9b13eaf1a8c6ce4ae73bf51884dabc9
Giorgio Giannotta <jorge_upa@msn.com>
2023-04-09 12:08:30 +0200

    Add json route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,12 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("json") { req -> Response in
+        let json = ["name": "John", "lastname": "Doe", "email": "johndoe@email.com", "hobbies" : ["Rugby", "TV", "NFL"]]
+            let data = try JSONSerialization.data(withJSONObject: json)
+            let body = Response.Body(data: data)
+            let response = Response(status: .ok, headers: HTTPHeaders([("Content-Type", "application/json")]), body: body)
+            return response
+        }
 }
